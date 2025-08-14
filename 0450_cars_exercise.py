4e9e4e486b4aca95a23994b92da9d9594f8067ec
Spark436 <66669@edu.campusvejle.dk>
2025-08-14 11:00:28 +0200

    0450 update

---
+++ 0450_cars_exercise.py
@@ -0,0 +1,39 @@
+"""
+Opgave "Cars":
+
+Som altid skal du læse hele opgavebeskrivelsen omhyggeligt, før du begynder at løse opgaven.
+
+Kopier denne fil til din egen løsningsmappe. Skriv din løsning ind i kopien.
+
+Definer en funktion drive_car(), der udskriver en bils motorlyd (f.eks. "roooaar")
+
+I hovedprogrammet:
+    Definer variabler, som repræsenterer antallet af hjul og den maksimale hastighed for 2 forskellige biler
+    Udskriv disse egenskaber for begge biler
+    Kald derefter funktionen drive_car()
+
+Hvis du ikke har nogen idé om, hvordan du skal begynde, kan du åbne 0460_cars_help.py og starte derfra.
+Hvis du går i stå, kan du spørge google, de andre elever, en AI eller læreren.
+Hvis du stadig er gået i stå, skal du åbne 0470_cars_solution.py og sammenligne den med din løsning.
+
+Når dit program er færdigt, skal du skubbe det til dit github-repository.
+"""
+
+
+def drive_car():
+    print("Motorlyd")
+
+car1_wheels = 4
+
+car1_speed = 50
+
+car2_wheels = 4
+
+car2_speed = 70
+
+print("car1", car1_wheels, car1_speed)
+
+print("car2", car2_wheels, car2_speed)
+
+drive_car()
+
