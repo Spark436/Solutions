37cd6ec31a1f9e7ebee6b16e925d2d773012f7c2
Spark436 <66669@edu.campusvejle.dk>
2025-10-01 14:53:35 +0200

    Update

--- 0630_rpg2_exercise.py
+++ 0630_rpg2_exercise.py
@@ -32,7 +32,7 @@ Når dit program er færdigt, skal du skubbe det til dit github-repository.
 import random
 
 class Character:
-    def __init__(self, name, max_health, _current_health, attackpower, attack_multiplier=1.0, mana=0, healpower=0, crit_chance=0, crit_damage=1.0):
+    def __init__(self, name, max_health, _current_health, attackpower, attack_multiplier=1.0, mana=0, healpower=0, crit_chance=0.0, crit_damage=1.0):
         self.name = name
         self.max_health = max_health
         self._current_health = _current_health
@@ -41,6 +41,7 @@ class Character:
         self.mana = mana
         self.healpower = healpower
         self.crit_chance = crit_chance
+        self.crit_damage = crit_damage
 
     def __repr__(self):
         return F"Name: {self.name},\n Max health: {self.max_health}, Current health: {self._current_health}, Attack power: {self.attackpower}"
@@ -68,11 +69,14 @@ class Healer(Character):
         other.get_healed(self)
 
 class Mage(Character):
-    def __init__(self, name, max_health, _current_health, attackpower, attack_multiplier, mana):
-        super().__init__(name, max_health, _current_health, attackpower, attack_multiplier=attack_multiplier, mana=mana)
+    def __init__(self, name, max_health, _current_health, attackpower, attack_multiplier, mana, crit_chance, crit_damage):
+        super().__init__(name, max_health, _current_health, attackpower, attack_multiplier=attack_multiplier, mana=mana, crit_chance=crit_chance, crit_damage=crit_damage)
 
     def fireball(self, other):
         damage = self.attackpower * self.attack_multiplier
+        crit = random.random()
+        if crit < self.crit_chance:
+            damage *= self.crit_damage
         self.hit(other, damage)
 
     def hit(self, other, damage=None):
@@ -84,15 +88,26 @@ class Barbarian(Character):
     def __init__(self, name, max_health, _current_health, attackpower, attack_multiplier, crit_chance, crit_damage):
         super().__init__(name, max_health, _current_health, attackpower, attack_multiplier=attack_multiplier, crit_chance=crit_chance, crit_damage=crit_damage)
 
+    def blind_rage(self, other):
+        damage = self.attackpower * 1.8
+        crit = random.random()
+        if crit < self.crit_chance:
+            damage *= self.crit_damage
+        self.hit(other, damage)
+
+
+
 
 ron = Character("Ron", 100, 100, 10)
 bon = Character("Bon", 100, 100,  10)
 ellen = Healer("Ellen", 90, 90, 0, 10)
-wizzy = Mage("Wizzy", 80, 80, 10, 1.5, 100)
+wizzy = Mage("Wizzy", 80, 80, 10, 1.5, 100, 0.2, 1.2)
+bonkus = Barbarian("Bonkus",110, 110, 10, 1.2, 0.4, 1.5)
 # ron.hit(bon)
 # print(bon)
 # ellen.heal(bon)
 # print(bon)
 # wizzy.fireball(bon)
-wizzy.fireball(bon)
+# wizzy.fireball(bon)
+bonkus.blind_rage(bon)
 print(bon)
