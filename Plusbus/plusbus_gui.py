0c584d9bcf831bff7f2c8081c3b7a606faa8733b
Spark436 <66669@edu.campusvejle.dk>
2026-02-02 15:10:20 +0100

    Update

--- Plusbus/plusbus_gui.py
+++ Plusbus/plusbus_gui.py
@@ -20,3 +20,41 @@ def clear_customer_entries():  # Clear entry boxes
     entry_customer_weight.delete(0, tk.END)
     entry_customer_destination.delete(0, tk.END)
     entry_customer_weather.delete(0, tk.END)
+
+    # region common functions
+    def refresh_treeview(tree, class_):  # Refresh treeview table
+        empty_treeview(tree)  # Clear treeview table
+        read_table(tree, class_)  # Fill treeview from database
+
+# region common widgets
+main_window = tk.Tk()  # Define the main window
+main_window.title('Plusbus')  # Text shown in the top window bar
+main_window.geometry("500x500")  # window size
+
+style = ttk.Style()  # Add style
+style.theme_use('default')  # Pick theme
+
+# region container widgets
+# Define Labelframe which contains all container related GUI objects (data table, labels, buttons, ...)
+frame_container = tk.LabelFrame(main_window, text="Container")  # https://www.tutorialspoint.com/python/tk_labelframe.htm
+frame_container.grid(row=0, column=0, padx=padx, pady=pady, sticky=tk.N)  # https://www.tutorialspoint.com/python/tk_grid.htm
+
+# Define data table (Treeview) and its scrollbar. Put them in a Frame.
+tree_frame_container = tk.Frame(frame_container)  # https://www.tutorialspoint.com/python/tk_frame.htm
+tree_frame_container.grid(row=0, column=0, padx=padx, pady=pady)
+tree_scroll_container = tk.Scrollbar(tree_frame_container)
+tree_scroll_container.grid(row=0, column=1, padx=0, pady=pady, sticky='ns')
+tree_container = ttk.Treeview(tree_frame_container, yscrollcommand=tree_scroll_container.set, selectmode="browse")  # https://docs.python.org/3/library/tkinter.ttk.html#treeview
+tree_container.grid(row=0, column=0, padx=0, pady=pady)
+tree_scroll_container.config(command=tree_container.yview)
+
+# Define the data table's formatting and content
+tree_container['columns'] = ("id", "last name", "contact")  # Define columns
+tree_container.column("#0", width=0, stretch=tk.NO)  # Format columns. Suppress the irritating first empty column.
+tree_container.column("id", anchor=tk.E, width=40)  # "E" stands for East, meaning Right. Possible anchors are N, NE, E, SE, S, SW, W, NW and CENTER
+tree_container.column("last name", anchor=tk.E, width=80)
+tree_container.column("contact", anchor=tk.W, width=200)
+
+if __name__ == "__main__":  # Executed when invoked directly. We use this so main_window.mainloop() does not keep our unit tests from running.
+    refresh_treeview(tree_container, dcd.Container)  # Load data from database
+    main_window.mainloop()  # Wait for button clicks and act upon them
