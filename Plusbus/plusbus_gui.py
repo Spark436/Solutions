c84f53350d60ed337efac4acab2282800b66cb4a
Spark436 <66669@edu.campusvejle.dk>
2026-02-17 14:32:48 +0100

    update

--- Plusbus/plusbus_gui.py
+++ Plusbus/plusbus_gui.py
@@ -17,6 +17,40 @@ def read_customer_entries():  # Read content of entry boxes
 
 def clear_customer_entries():  # Clear entry boxes
     entry_customer_id.delete(0, tk.END)  # Delete text in entry box, beginning with the first character (0) and ending with the last character (tk.END)
-    entry_customer_weight.delete(0, tk.END)
-    entry_customer_destination.delete(0, tk.END)
-    entry_customer_weather.delete(0, tk.END)
+    entry_customer_last_name.delete(0, tk.END)
+    entry_customer_contact_info.delete(0, tk.END)
+
+# region common widgets
+main_window = tk.Tk()  # Define the main window
+main_window.title('AspIT S2: DanskCargo')  # Text shown in the top window bar
+main_window.geometry("1200x500")  # window size
+
+style = ttk.Style()  # Add style
+style.theme_use('default')  # Pick theme
+
+# region container widgets
+# Define Labelframe which contains all container related GUI objects (data table, labels, buttons, ...)
+frame_container = tk.LabelFrame(main_window, text="Container")  # https://www.tutorialspoint.com/python/tk_labelframe.htm
+frame_container.grid(row=0, column=0, padx=padx, pady=pady, sticky=tk.N)  # https://www.tutorialspoint.com/python/tk_grid.htm
+
+# Define data table (Treeview) and its scrollbar. Put them in a Frame.
+tree_frame_container = tk.Frame(frame_container)  # https://www.tutorialspoint.com/python/tk_frame.htm
+tree_frame_container.grid(row=0, column=0, padx=padx, pady=pady)
+tree_scroll_container = tk.Scrollbar(tree_frame_container)
+tree_scroll_container.grid(row=0, column=1, padx=0, pady=pady, sticky='ns')
+tree_container = ttk.Treeview(tree_frame_container, yscrollcommand=tree_scroll_container.set, selectmode="browse")  # https://docs.python.org/3/library/tkinter.ttk.html#treeview
+tree_container.grid(row=0, column=0, padx=0, pady=pady)
+tree_scroll_container.config(command=tree_container.yview)
+
+# Define the data table's formatting and content
+tree_container['columns'] = ("id", "last name", "Contact info")  # Define columns
+tree_container.column("#0", width=0, stretch=tk.NO)  # Format columns. Suppress the irritating first empty column.
+tree_container.column("id", anchor=tk.E, width=40)  # "E" stands for East, meaning Right. Possible anchors are N, NE, E, SE, S, SW, W, NW and CENTER
+tree_container.column("weight", anchor=tk.E, width=80)
+tree_container.column("destination", anchor=tk.W, width=200)
+tree_container.heading("#0", text="", anchor=tk.W)  # Create column headings
+tree_container.heading("id", text="Id", anchor=tk.CENTER)
+tree_container.heading("weight", text="Last name", anchor=tk.CENTER)
+tree_container.heading("destination", text="Contact info", anchor=tk.CENTER)
+tree_container.tag_configure('oddrow', background=oddrow)  # Create tags for rows in 2 different colors
+tree_container.tag_configure('evenrow', background=evenrow)
